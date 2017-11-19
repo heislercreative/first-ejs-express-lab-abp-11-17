a1ebd9c9977ae8079c17f3b815d2498232fdc1d0
Brennan Heisler <learn@heislercreative.com>
2017-11-19 18:05:14 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var app = express()
 // Build Your Route Here
 app.get("/", function(req, resp){
   const welcomeText = "Welcome to HTML"
-  resp.render("index")
+
+  resp.render("index, {"welcomeText": welcomeText}")
 })
 
 module.exports = app
