c53be4bc09c93458933f914e3407f59ebe67b88a
Brennan Heisler <learn@heislercreative.com>
2017-11-19 18:08:17 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ var app = express()
 // Build Your Route Here
 app.get("/", function(req, resp){
   const welcomeText = "Welcome to HTML"
+  const favoriteThings = ["Code," "Music", "People"]
 
-  resp.render("index, {"welcomeText": welcomeText}")
+  resp.render("index, {
+    "welcomeText": welcomeText,
+    name: "Avi",
+    colors: ["Red", "Yellow", "Blue"],
+    favoriteThings: favoriteThings
+  })
 })
 
 module.exports = app
