d28769b35e66b455bfb205ff00d3098e7ef3e435
Brennan Heisler <learn@heislercreative.com>
2017-11-19 18:09:59 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get("/", function(req, resp){
   const welcomeText = "Welcome to HTML"
   const favoriteThings = ["Code," "Music", "People"]
 
-  resp.render("index, {
+  resp.render("index", {
     "welcomeText": welcomeText,
     name: "Avi",
     colors: ["Red", "Yellow", "Blue"],
