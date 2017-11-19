d56b20a4a858adae0e340de1ca404458d5b1140f
Brennan Heisler <learn@heislercreative.com>
2017-11-19 18:43:47 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ app.get("/", function(req, resp){
   const welcomeText = "Hello, World!"
   const favoriteThings = ["Code", "Music", "People"]
 
-  resp.render("index", {
-    "welcomeText": welcomeText
+  resp.render("index", welcomeText)
   })
 })
 
